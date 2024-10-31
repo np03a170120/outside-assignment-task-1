e9f6c2b76c34554bbb8e8d7a56d150b4680b41d6
np03a170120 <czon.herald75@gmail.com>
2024-10-31 10:44:07 +0545

    fix:animation on hamburger & ui fixes inmobile menu

--- app/toggleHamburgerMenu.js
+++ app/toggleHamburgerMenu.js
@@ -1,12 +1,18 @@
-export function toggleHamburgerMenu () {
+export function toggleHamburgerMenu() {
   const menu = document.querySelector(".nav_links");
   const toggleIcon = document.querySelector("#toggleIcon");
   const menuIcon = "./image/icons/menu.svg";
   const crossIcon = "./image/icons/close.svg";
 
   toggleIcon.addEventListener("click", () => {
-    menu.classList.toggle("show-links");
-
+    if (menu.classList.contains("show-links")) {
+      menu.classList.add("hide-links");
+      setTimeout(() => {
+        menu.classList.remove("show-links", "hide-links");
+      }, 300);
+    } else {
+      menu.classList.add("show-links");
+    }
     toggleIcon.src = toggleIcon.src.endsWith("menu.svg") ? crossIcon : menuIcon;
   });
-};
+}
