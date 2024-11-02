64cbe880411b4b9ec997b9b326daa65090329be6
dreamCode <czon.herald75@gmail.com>
2024-11-02 14:42:43 +0545

    feat: desktop version

--- app/slider.js
+++ app/slider.js
@@ -6,4 +6,18 @@ export const slider = new Swiper(".swiper", {
   slidesPerView: 3,
   spaceBetween: 10,
   loop: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+  },
 });
